8209afd2e08e1deddcae2314185f4e29096beae7
truonghoanglong <57944514+truonghoanglong@users.noreply.github.com>
2021-10-07 16:51:50 +0700

    fix

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,8 +1,14 @@
 const $ = document.querySelector.bind(document)
 const $$ = document.querySelectorAll.bind(document)
 
-document.addEventListener('DOMContentLoaded',()=>{
-    
-})
+document.addEventListener('DOMContentLoaded',function(){
+    const video = $('.trailer')
+    const play = $('.play')
+    play.addEventListener('click', function(){
+        video.classList.add('active');
+        console.log('click')
+    },false)
+},false)
+
 
 
